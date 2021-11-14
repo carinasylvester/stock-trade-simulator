f0f099c40d9296bb57f595e1bcb16801419e7440
carinasylvester <carina.sylvester@outlook.com>
2021-11-13 22:14:41 -0500

    More error reducers

--- client/src/reducers/error.js
+++ client/src/reducers/error.js
@@ -1,6 +1,6 @@
 import { AUTH_ERROR_OCCURRED, MARKET_ERROR_OCCURRED, PURCHASED_ERROR_OCCURRED, USER_ERROR_OCCURRED, TRANSACTION_ERROR_OCCURRED, LOG_ERROR_OCCURRED } from '../constants/actions';
 
-// errors for authentication
+// Authentication errors
 const authErrorsReducer = (state = [], action) => {
   switch (action.type) {
     case AUTH_ERROR_OCCURRED:
@@ -10,7 +10,27 @@ const authErrorsReducer = (state = [], action) => {
   }
 };
 
-// errors for stock market
+// Transaction errors
+const transactionErrorsReducer = (state = [], action) => {
+    switch (action.type) {
+      case TRANSACTION_ERROR_OCCURRED:
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+  
+// Action Log errors
+  const logsErrorsReducer = (state = [], action) => {
+    switch (action.type) {
+      case LOG_ERROR_OCCURRED:
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+
+// Stockmarket errors
 const marketErrorsReducer = (state = [], action) => {
   switch (action.type) {
     case MARKET_ERROR_OCCURRED:
@@ -18,3 +38,26 @@ const marketErrorsReducer = (state = [], action) => {
     default:
       return state;
   }
+};
+
+// User errors
+const userErrorsReducer = (state = [], action) => {
+  switch (action.type) {
+    case USER_ERROR_OCCURRED:
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+// Purchased Stocks errors
+const purchasedErrorsReducer = (state = [], action) => {
+    switch (action.type) {
+      case PURCHASED_ERROR_OCCURRED:
+        return action.payload;
+      default:
+        return state;
+    }
+  };
+
+export { authErrorsReducer, marketErrorsReducer, purchasedErrorsReducer, userErrorsReducer, transactionErrorsReducer, logsErrorsReducer };
