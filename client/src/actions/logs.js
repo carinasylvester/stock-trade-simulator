862b21f8a12a95bb608135cbfb12720971c1b76a
carinasylvester <carina.sylvester@outlook.com>
2021-11-13 21:42:05 -0500

    Added actions

---
+++ client/src/actions/logs.js
@@ -0,0 +1,9 @@
+import { fetchLogs } from '../api/index.js';
+import { GET_ALL_LOGS, LOG_ERROR_OCCURRED } from '../constants/actions';
+
+// GET /logs
+export const getLogs = () => async (dispatch) => {
+  try {
+    const { data } = await fetchLogs();
+    dispatch({ type: GET_ALL_LOGS, payload: data });
+  } catch (error) {
