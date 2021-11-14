fe984badb02e657cac8c90f68b72ab10d1b40249
carinasylvester <carina.sylvester@outlook.com>
2021-11-13 22:11:15 -0500

    Reducers for user actions

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -10,3 +10,21 @@ const authReducer = (state = { authData: null }, action) => {
       localStorage.removeItem('profile');
       return { ...state, authData: null, errors: null };
     case USER_INFO:
+      const userObject = JSON.parse(localStorage.getItem("profile"));
+      userObject.result.coins = action?.data.coins;
+      localStorage.setItem('profile', JSON.stringify(userObject));
+      return { ...state, authData: action.data, errors: null };
+    case USER_UPDATE_NAME:
+      const userObjectNewName = JSON.parse(localStorage.getItem("profile"));
+      userObjectNewName.result.name = action?.data.name;
+      localStorage.setItem('profile', JSON.stringify(userObjectNewName));
+      return { ...state, authData: action.data, errors: null };
+    case DELETE_USER:
+      localStorage.removeItem('profile');
+      return { ...state, authData: null, errors: null };
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
