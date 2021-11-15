f3b8d97de14b0c50dc8a1013be24bbed29d60c6e
carinasylvester <carina.sylvester@outlook.com>
2021-11-14 21:29:51 -0500

    Light/Dark mode toggle

--- client/src/components/ToggleTheme.jsx
+++ client/src/components/ToggleTheme.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { ThemeContext } from "../../contexts/ThemeContext";
+import { ThemeContext } from "../contexts/ThemeContext";
 
 const ToggleTheme = (props) => {
   const { styleSet } = props;
